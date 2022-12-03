8e6654d47dba2d4301233496a436fa4ce84de7cf
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-03 09:55:09 +0000

    Addde array for entities and instance of hxd.App

--- src/ng/NGGame.hx
+++ src/ng/NGGame.hx
@@ -20,14 +20,36 @@ class NGGame
       **/
     var title  : String;
 
-    public function new(width:Int, height:Int, title:String)
+    /**
+      Instance of Heaps App class.
+      **/
+    var app  : hxd.App;
+
+    /**
+      Array of all of the entities available
+      **/
+    var entities : Array<NGObject>;
+
+    public function new(width:Int, height:Int, title:String, app:hxd.App)
     {
         this.width  = width;
         this.height = height;
         this.title  = title;
+        this.app = app;
+        this.entities = new Array<NGObject>();
     }
 
-    override function update(dt:Float) {
+    public function addEntity(entity:NGObject)
+    {
+        this.entities.push(entity);
+    }
+
+    public function update(dt:Float) {
         trace("Updating Game");
+
+        for(e in this.entities)
+        {
+            e.update(dt);
+        }
     }
 }
