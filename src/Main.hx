fc8af9c1092c7bef18dd38d5891449db2369f85b
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-03 08:48:15 +0000

    First Commit

---
+++ src/Main.hx
@@ -0,0 +1,21 @@
+package;
+
+class Main extends hxd.App {
+
+    var bmp : h2d.Bitmap;
+
+    override function init() {
+        var tile = h2d.Tile.fromColor(0x0000FF, 100, 100);
+        bmp = new h2d.Bitmap(tile, s2d);
+        bmp.x = s2d.width * 0.5;
+        bmp.y = s2d.height * 0.5;
+    }
+
+    override function update(dt:Float) {
+        bmp.rotation += 0.1;
+    }
+
+    static function main() {
+        new Main();
+    }
+}
