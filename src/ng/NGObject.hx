124db54715356811b40e04d9299efd8979f83e65
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-03 09:30:07 +0000

    Added width, height, direction x and direction y properties to NGObject.

--- src/ng/NGObject.hx
+++ src/ng/NGObject.hx
@@ -15,8 +15,28 @@ class NGObject extends NGBasic
       The x position of the object **/
     public var y : Float = 0;
 
+    /**
+      The width of the object **/
+    public var w : Float = 10;
+
+    /**
+      The height of the object **/
+    public var h : Float = 10;
+
+    /**
+      The x direction **/
+    public var dx : Float = 1;
+
+    /**
+      The y direction **/
+    public var dy : Float = 1;
+
     public function new()
     {
         super();
     }
+
+    /**
+      Base update method that should be overriden **/
+    public function update(dt:Float) {}
 }
